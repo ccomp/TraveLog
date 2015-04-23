1c9a4fdd74cf5fc931b2d082f5386852cde4e6bd
Cyron Completo <Main@res2-201-217-dhcp.int.colorado.edu>
2015-04-22 18:41:58 -0600

    Initial Commit

---
+++ TraveLog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TraveLog
+//
+//  Created by Cyron Completo on 4/22/15.
+//  Copyright (c) 2015 Cyron Completo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
