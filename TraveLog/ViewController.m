414e17a526077ac60f41de81101b5631db5b9952
Cyron Completo <Main@res2-203-104-dhcp.int.colorado.edu>
2015-05-01 19:22:37 -0600

    locations fetched and map implemented, resize works

--- TraveLog/ViewController.m
+++ TraveLog/ViewController.m
@@ -35,16 +35,37 @@
     [self.locationManager startUpdatingLocation];
 }
 
+- (MKCoordinateRegion)createRegion {
+    CLLocationCoordinate2D topLeftCoord;
+    topLeftCoord.latitude = -90;
+    topLeftCoord.longitude = 180;
+
+    CLLocationCoordinate2D bottomRightCoord;
+    bottomRightCoord.latitude = 90;
+    bottomRightCoord.longitude = -180;
+    
+    for (id key in locDict) {
+        topLeftCoord.longitude = fmin(topLeftCoord.longitude, ((CLLocation *)[locDict objectForKey:key]).coordinate.longitude);
+        topLeftCoord.latitude = fmax(topLeftCoord.latitude, ((CLLocation *)[locDict objectForKey:key]).coordinate.latitude);
+        bottomRightCoord.longitude = fmax(bottomRightCoord.longitude, ((CLLocation *)[locDict objectForKey:key]).coordinate.longitude);
+        bottomRightCoord.latitude = fmin(bottomRightCoord.latitude, ((CLLocation *)[locDict objectForKey:key]).coordinate.latitude);
+    }
+    
+    MKCoordinateRegion region;
+    region.center.latitude = topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) * 0.5;
+    region.center.longitude = topLeftCoord.longitude + (bottomRightCoord.longitude - topLeftCoord.longitude) * 0.5;
+    
+    // Add a little extra space on the sides
+    region.span.latitudeDelta = fabs(topLeftCoord.latitude - bottomRightCoord.latitude) * 1.1;
+    region.span.longitudeDelta = fabs(bottomRightCoord.longitude - topLeftCoord.longitude) * 1.1;
+    return region;
+}
+
 -(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
 {
-    int x = 2500, y = 2500;
-    CLLocationCoordinate2D myLocation = [userLocation coordinate];
-    if (locDict.count != 0)
-    {
-        
-    }
-    MKCoordinateRegion zoomRegion = MKCoordinateRegionMakeWithDistance(myLocation, x, y);
-    [self.myMapView setRegion:zoomRegion animated:YES];
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 2500, 2500);
+    if (locDict.count == 0) [self.myMapView setRegion:region animated:YES];
+    else [self.myMapView setRegion:[self createRegion] animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
